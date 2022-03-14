f2246b479fe4147c7bc34eabbfcde7fc404f7145
Celestin-Cordos <63093619+Celestin-Cordos@users.noreply.github.com>
2022-03-14 10:08:41 +0100

    removed the prettierrc
    also added an input

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -9,6 +9,7 @@ function Home() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <input type="text" id="name"/>
         <a
           className="App-link"
           href="https://reactjs.org"
